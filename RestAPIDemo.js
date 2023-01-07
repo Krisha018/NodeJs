ad8c092f67556fb8f984660256687945503ac687
Padaliya Krisha <108071979+Krisha018@users.noreply.github.com>
2023-01-07 19:06:49 +0530

    Add files via upload

--- RestAPIDemo.js
+++ RestAPIDemo.js
@@ -1,24 +1,46 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Car = require("./model/car");
+const express = require('express');
+const mongoose = require('mongoose');
+const Faculty = require('./model/Faculty');
 const bodyParser = require('body-parser')
-
-mongoose.connect('mongodb+srv://Krisha:Krisha@cluster0.z7bomx3.mongodb.net/supercar?retryWrites=true&w=majority').then(() => {
+const cors = require('cors');
+mongoose.connect('mongodb+srv://Krisha:<password>@cluster0.z7bomx3.mongodb.net/collages?retryWrites=true&w=majority').then(() => {
     const app = express();
     app.use(bodyParser.urlencoded({ extended: false }))
-
-    app.get('/carsdetails', async(req, res) => {
-        const data = await Car.find();
+    app.use(cors());
+    app.get('/faculties', async(req, res) => {
+        const data = await Faculty.find();
         res.send(data);
     });
 
-    app.get('/car/:id', async(req, res) => {
-        const data = await Car.findOne({ CarName: req.params.id });
+    app.get('/faculty/:id', async(req, res) => {
+        const data = await Faculty.findOne({ FacultyInitial: req.params.id });
         res.send(data);
     });
 
+    app.post('/faculty', async(req, res) => {
+        const fac = new Faculty();
+        fac.FacultyInitial = req.body.FI;
+        fac.FacultyName = req.body.FN;
+        fac.FacultyAge = req.body.FA;
+        const data = await fac.save();
+        res.send(data);
+    });
+
+    app.put('/faculty/:id', async(req, res) => {
+        const data = await Faculty.findOne({ FacultyInitial: req.params.id });
+        data.FacultyName = req.body.FN;
+        data.FacultyAge = req.body.FA;
+
+        await data.save();
+        res.send(data);
+    });
+
+    app.delete('/faculty/:id', async(req, res) => {
+        const data = await Faculty.deleteOne({ FacultyInitial: req.params.id });
+        res.send(data);
+    });
 
     app.listen(3003, () => {
-        console.log("Server at start @3003");
+        console.log("Server started at @ 3003");
     })
-})
+});
